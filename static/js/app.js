acca627bf763c0822e50a967d54c62c653669900
Dystopian Fields <dt@gact.space>
2018-08-12 16:43:01 +0300

    Add footer
    * Fix post date
    * Fix about attribution

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
     'use strict';
 
+    // Add current copyright year
+    //const current_year = '-' + new Date().getFullYear();
+    //$('#copy').text(current_year);
 });
